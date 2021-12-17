47c0b03959f5b3a58d9b04b0f8068a868cac6af3
lythanhnam09 <lythanhnam09@gmail.com>
2021-12-17 18:57:24 +0700

    Fix literal expression in sql_command

--- lib/util/sql_command.py
+++ lib/util/sql_command.py
@@ -30,21 +30,17 @@ def _parse_expr_list(arg:list) -> str:
                     if (expr[1] in ['=', '<>', '>', '<', 'in', 'like']):
                         ls_expr.append(f'{expr[0]} {expr[1]} ?')
                     else:
-                        if (type(expr[1]) in [int, float]):
-                            ls_expr.append(f'{expr[0]} = {expr[1]}')
-                        elif (type(expr[1]) == str):
-                            ls_expr.append(f'{expr[0]} = \'{expr[1]}\'')
+                        if (type(expr[1]) in [int, float, str]):
+                            ls_expr.append(f'{expr[0]} = {expr[1]!r}')
                         elif (type(expr[1]) == list):
                             ls_expr.append(f'{expr[0]} = {expr[1][0]}')
                 elif (len(expr) == 3):
-                    value = expr[2]
-                    if (type(expr[2]) in [int, float]):
-                        expr[2] = f'{expr[2]}'
-                    elif (type(expr[2]) == str):
-                        expr[2] = f'\'{expr[2]}\''
+                    if (type(expr[2]) in [int, float, str]):
+                        expr[2] = f'{expr[2]!r}'
+                        ls_expr.append(' '.join(expr))
                     elif (type(expr[2]) == list):
-                        ls_expr.append(f'{expr[0]} = {expr[2][0]}')
-                    ls_expr.append(' '.join(expr))
+                        ls_expr.append(' '.join([expr[0], expr[1], expr[2][0]]))
+                    
                 last_type = 'list'
             index += 1
 
@@ -102,10 +98,49 @@ def select_join(table:list, join_on:str, columns:list = None, where:list = None,
         result += 'select ' + ', '.join(columns)
 
     if (len(table) == 2):
-        result += ('\n' if line_break else ' ') + f'from {table[0]} join {table[1]} on '
-        result += join_on
+        result += ('\n' if line_break else ' ') + f'from {table[0]} join {table[1]} on {join_on}'
     else:
         raise Exception('The number of join table in list must be 2, otherwise better use select()')
+
+    if (where != None):
+        result += ('\n' if line_break else ' ') + 'where '
+        result += _parse_expr_list(where)
+
+    if (group_by != None):
+        result += ('\n' if line_break else ' ') + 'group by '
+        result += ', '.join(group_by)
+
+    if (having != None):
+        result += ('\n' if line_break else ' ') + 'having '
+        result += _parse_expr_list(having)
+
+    if (order_by != None):
+        result += ('\n' if line_break else ' ') + 'order by '
+        if (type(order_by) == list):
+            result += ' '.join(order_by)
+        else:
+            result += order_by
+
+    if (limit != None):
+        result += ('\n' if line_break else ' ') + f'limit {limit}'
+
+    if (offset != None):
+        result += ('\n' if line_break else ' ') + f'offset {offset}'
+
+    return result
+
+def select_join_3(table:list, join_name:str, join_on:list, columns:list = None, where:list = None, group_by:list = None, having:list = None, order_by:list = None, limit:int = None, offset:int = None, line_break = False) -> str:
+    result = ''
+
+    if (columns == None):
+        result += 'select *'
+    else:
+        result += 'select ' + ', '.join(columns)
+
+    if (len(table) == 3):
+        result += ('\n' if line_break else ' ') + f'from ({table[0]} join {table[1]} on {join_on[0]}) {join_name} join {table[2]} on {join_on[1]}'
+    else:
+        raise Exception('The number of join table in list must be 3, otherwise better use select() or select_join()')
 
     if (where != None):
         result += ('\n' if line_break else ' ') + 'where '
